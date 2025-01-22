7618b7523b4d56cf38aeff495e61b16006d813d5
rondinellenery <rondipaper@gmail.com>
2025-01-22 11:30:13 -0300

    Variaveis Estudo

---
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,27 @@
+public class TipoVariaveis {
+
+        /**
+         * @param args
+         * @throws Exception
+         */
+        public static void main(String[] args) throws Exception { 
+            short numeroCurto = 1;
+            int numeroNormal = numeroCurto;
+            short numeroCurto2 = (short) numeroNormal;
+            
+            String meuNome = "Rondinelle Nery";
+
+            System.out.println(meuNome);
+
+            int numero =5;
+
+            numero = 10;
+
+            System.out.println(numero);
+
+            double VALOR_DE_PI = 3.14;
+            System.out.print(VALOR_DE_PI);
+
+        }
+
+}
